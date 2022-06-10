58d9230c3cadaf96bea068bc65a81f9c0bb3ea54
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-06-10 09:30:29 -0700

    Create the localbusiness class and set the properties

---
+++ LocalBusinessApi/Models/LocalBusiness.cs
@@ -0,0 +1,14 @@
+namespace LocalBusinessApi.Models
+{
+  public class LocalBusines
+  {
+    public int LocalBusinessApiId { get; set; }
+    public string  Name { get; set; }
+    public string Owner { get; set; }
+    public string Type { get; set; }
+    public string Location { get; set; }
+    public int Rating { get; set; }
+
+
+  }
+}
