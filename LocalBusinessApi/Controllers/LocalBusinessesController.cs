bbc15e02d234d285f1351e1ccee4b316ad917f4b
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-06-10 11:18:41 -0700

    Create a all the CRUD operations

--- LocalBusinessApi/Controllers/LocalBusinessesController.cs
+++ LocalBusinessApi/Controllers/LocalBusinessesController.cs
@@ -41,12 +41,50 @@ namespace LocalBusinessApi.Controllers
     {
       _db.LocalBusinesses.Add(localbusiness);
       await _db.SaveChangesAsync();
-      return CreatedAtAction("Post",new{id=localbusiness.LocalBusinessId},localbusiness);
-
+      return CreatedAtAction(nameof(GetBusiness),new{id=localbusiness.LocalBusinessId},localbusiness);
     } 
-    
+    [HttpPut("{id}")]
+    public async Task<ActionResult> Put(int id,LocalBusiness localBusiness)
+    { 
+      if(id!=localBusiness.LocalBusinessId)
+      {
+        return BadRequest();
+      }
+      _db.Entry(localBusiness).State =EntityState.Modified;
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch(DbUpdateConcurrencyException)
+      {
+        if(!LocalBusinessExists(id))
+        {
+          return  NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+      return NoContent();
 
-
+    }
+    private bool LocalBusinessExists(int id )
+    {
+      return _db.LocalBusinesses.Any(entry=>entry.LocalBusinessId ==id);
+    }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+      var localBusiness =await _db.LocalBusinesses.FindAsync(id);
+      if(localBusiness == null)
+      {
+        return NotFound();
+      }
+      _db.LocalBusinesses.Remove(localBusiness);
+      await _db.SaveChangesAsync();
+      return NoContent();
+    }
   }
 }
 
